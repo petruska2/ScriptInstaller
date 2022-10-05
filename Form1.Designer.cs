903c14f99d66db7e03b514611a777cf731a42303
petruska2 <113216726+petruska2@users.noreply.github.com>
2022-10-05 09:13:43 -0500

    Original Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Script_Installer_
+{
+    partial class MainWindoww
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindoww));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FilePath = new System.Windows.Forms.TextBox();
+            this.Close = new System.Windows.Forms.Button();
+            this.Install = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(235, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Script to Install:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FilePath
+            // 
+            this.FilePath.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.FilePath.Location = new System.Drawing.Point(15, 76);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.ReadOnly = true;
+            this.FilePath.Size = new System.Drawing.Size(210, 13);
+            this.FilePath.TabIndex = 2;
+            this.FilePath.TextChanged += new System.EventHandler(this.FilePath_TextChanged);
+            // 
+            // Close
+            // 
+            this.Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Close.ForeColor = System.Drawing.Color.White;
+            this.Close.Location = new System.Drawing.Point(235, 99);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(53, 29);
+            this.Close.TabIndex = 3;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Install
+            // 
+            this.Install.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Install.ForeColor = System.Drawing.Color.White;
+            this.Install.Location = new System.Drawing.Point(172, 99);
+            this.Install.Name = "Install";
+            this.Install.Size = new System.Drawing.Size(53, 29);
+            this.Install.TabIndex = 4;
+            this.Install.Text = "Install";
+            this.Install.UseVisualStyleBackColor = true;
+            this.Install.Click += new System.EventHandler(this.Install_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, -5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 55);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Result
+            // 
+            this.Result.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Result.Location = new System.Drawing.Point(15, 99);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.Size = new System.Drawing.Size(151, 29);
+            this.Result.TabIndex = 6;
+            this.Result.TextChanged += new System.EventHandler(this.Result_TextChanged);
+            // 
+            // MainWindoww
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(130)))), ((int)(((byte)(101)))));
+            this.ClientSize = new System.Drawing.Size(300, 140);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Install);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.FilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindoww";
+            this.Text = "ScriptInstaller+";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FilePath;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button Install;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Principal;
+
+namespace Script_Installer_
+{
+    public partial class MainWindoww : Form
+    {
+
+        public MainWindoww()
+        {
+            InitializeComponent();
+        }
+
+public static bool IsAdministrator()
+    {
+        using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+        {
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+    private void button1_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    FilePath.Text = file;
+                }
+                catch (System.IO.IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FilePath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Install_Click(object sender, EventArgs e)
+        {
+            const string LOCATION = @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp";
+            string Path = FilePath.Text;
+            string FileName = Path.Split('\\').Last();
+            string NewPath = LOCATION + FileName;
+            if (!(IsAdministrator()))
+            {
+                Result.Text = "Please run as Administrator";
+            }
+            else if (Path == "")
+            {
+                Result.Text = "Please select a file";
+            } 
+            else if (!(File.Exists(Path)))
+            {
+                Result.Text = "Invalid Selection";
+            }
+            else
+            {
+                if (File.Exists(NewPath))
+                {
+                    try
+                    {
+                        File.Delete(NewPath);
+                    }
+                    catch (Exception)
+                    {
+                        Result.Text = "Unexpected Error deleting original script";
+                    }
+                }
+                try
+                {
+                    File.Move(Path, NewPath);
+                    Result.Text = "Success";
+                }
+                catch (Exception) 
+                {
+                    Result.Text = "Unexpected Error moving " + Path + " to " + NewPath;
+                }
+            }
+        }
+
+        private void Result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
